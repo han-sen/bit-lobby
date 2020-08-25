7260feea8ef1d677dd6a4a48e7a30b7bc32370c8
Michael Hansen <michaelandhansen@gmail.com>
2020-08-25 02:00:21 -0400

    added room link copy to clipboard

--- views/components/RoomDetails.jsx
+++ views/components/RoomDetails.jsx
@@ -4,10 +4,16 @@ const RoomDetails = (props) => {
     const { name, description, privateRoom } = props;
     return (
         <div className="room_details_wrap">
-            <h2 className="room_name">{name}</h2>
+            <h2 className="room_name">
+                {name}{" "}
+                <span className="room_link">
+                    <i class="fas fa-link"></i>
+                    <input type="text" id="room_link_text" />
+                </span>
+            </h2>
             {privateRoom ? (
                 <li className="room_privacy private">
-                    <i className="fas fa-eye-slash"></i>private room
+                    <i class="fas fa-ghost"></i>private room
                 </li>
             ) : (
                 <li className="room_privacy public">
